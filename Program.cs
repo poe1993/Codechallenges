89d9263156cda7d99c6e812371126c6bc562fcd2
poe1993 <poe1993@yahoo.com>
2020-08-24 16:21:35 -0400

    Names are above each set of code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿//Count Odd numbers below n:
+//using System;
 
-namespace Codechallenges
+//public static class Kata
+//{
+//public static ulong OddCount(ulong n)
+//{
+//var countOdds = n/2;
+//return countOdds;
+//}
+//}
+
+
+//Rock, Paper, Scissor:
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text.RegularExpressions;
+
+//public class Kata
+//{
+//public string Rps(string p1, string p2)
 {
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      Console.WriteLine("Welcome to C#");
-    }
-  }
-}
+    //if(p1 == p2)
+    //{return "Draw!";}
+    //if((p1 == "rock" && p2 == "scissors" ) || (p1 == "paper" && p2 == "rock") || (p1 == "scissors" && p2 == "paper"))
+    //{return "Player 1 won!";}
+    //else
+    //{
+    //return "Player 2 won!";
+    //  }
+    //}
+    //}
